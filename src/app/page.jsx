6c9f02dc6779c7705136d60dda31bad5ef79e33f
Asad Ali <itsasadali5@gmail.com>
2025-09-30 20:24:04 +0500

    folder structure

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,10 @@
+import Hero from "@/sections/Hero";
 
 const Home = () => {
   return (
-    <main className="text-2xl text-center p-4 bg-red-300 font-bold">
-      Portfolio
-    </main>
+   <>
+   <Hero/>
+   </>
   );
 };
 
