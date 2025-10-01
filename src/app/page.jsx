1e0d6743ce12b3a1126f0848993c3c63a4617cb3
Asad Ali <itsasadali5@gmail.com>
2025-10-01 15:25:37 +0500

    added logo section and navbar and feature section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,17 @@
 import Hero from "@/sections/Hero";
 import ShowcaseSection from "@/sections/ShowcaseSection";
+import NavBar from "./components/NavBar";
+import FeatureCards from "@/sections/FeatureCards";
+import LogoSection from "@/sections/LogoSection";
 
 const Home = () => {
   return (
    <>
+   <NavBar/>
    <Hero/>
    <ShowcaseSection/>
+   <LogoSection/>
+   <FeatureCards/>
    </>
   );
 };
